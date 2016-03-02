34db1ba4ee675ec4385f539461cd1eee20fe771f
Kevin Hardy <console.beep@gmail.com>
2016-03-02 15:53:11 -0700

    Main page CSS done

--- www/js/mainController.js
+++ www/js/mainController.js
@@ -2,15 +2,23 @@ angular.module('photoBombApp').controller('mainController', ['$scope', '$route',
     function ($scope, $route) {
         
         
+        /*
+            On success of taking the picture
+        */
         $scope.photoSuccessCallback = function () {
             alert("photo success!");
         }
         
+        /*
+            On failure to take the picture
+        */
         $scope.photoFailCallback = function () {
             alert("photo failure!");
         }
         
-        
+        /*
+            Take a picture with the device's camera
+        */
         $scope.takePicture = function() {
             // Take picture using device camera and retrieve image as base64-encoded string
             navigator.camera.getPicture($scope.photoSuccessCallback, $scope.photoFailCallback, 

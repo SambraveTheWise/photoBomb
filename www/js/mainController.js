17f037bdca5f03e34de92bd5b4270f4f2dfdcb84
Kevin Hardy <console.beep@gmail.com>
2016-03-02 20:51:02 -0700

    Photos taken now appear in app, pixel data is starting to be manipulated

--- www/js/mainController.js
+++ www/js/mainController.js
@@ -1,19 +1,57 @@
 angular.module('photoBombApp').controller('mainController', ['$scope', '$route', 
     function ($scope, $route) {
         
+        var photoWidth = 600;
+        var photoHeight = 800;
+        
+        /*
+            Ensures that the actual size of the canvas (not necessarily
+            the displayed size of the canvas) is the same dimensions as
+            the photo that will be taken
+        */
+        var theCanvas = document.getElementById("theCanvas");
+        theCanvas.width = photoWidth;
+        theCanvas.height = photoHeight;
+        var canvasContext = theCanvas.getContext("2d");
         
         /*
             On success of taking the picture
         */
-        $scope.photoSuccessCallback = function () {
-            alert("photo success!");
+        $scope.photoSuccessCallback = function (imgData) {
+            
+            imgData = "data:image/jpeg;base64," + imgData;
+            
+            var theImage = new Image();
+            theImage.src = imgData;
+            
+            theImage.onload = function() {
+                
+                // draw the image to the canvas
+                canvasContext.drawImage(theImage, 0, 0, photoWidth, photoHeight);
+                
+                // get the image data to be used in background removal
+                var pixelData = canvasContext.getImageData(0, 0, photoWidth, photoHeight);
+
+                var numPixelParts = pixelData.data.length;
+                for (i = 0; i < numPixelParts; i += 4) {
+                    for (j = 0; j < 4; j++) {
+                        
+                        // 2 is the blue part of the pixel
+                        if (j == 2) {
+                            pixelData.data[i + j] = 220;
+                        }
+                    }
+                }
+                
+                canvasContext.putImageData(pixelData, 0, 0);
+            }
         }
         
         /*
             On failure to take the picture
         */
         $scope.photoFailCallback = function () {
-            alert("photo failure!");
+            alert("Camera failure!");
         }
         
         /*
@@ -25,8 +63,8 @@ angular.module('photoBombApp').controller('mainController', ['$scope', '$route',
                 { 
                     quality: 100,
                     destinationType: Camera.DestinationType.DATA_URL,
-                    targetWidth: 600,
-                    targetHeight: 800,
+                    targetWidth: photoWidth,
+                    targetHeight: photoHeight,
                     correctOrientation: true
                 }
             );   
